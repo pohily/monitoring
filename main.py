6b4f99c429f148f9d54bd1de9fcbb6c28951d9b3
Михаил А. Похилый <m.pohilyj@4slovo.ru>
2020-05-04 13:05:19 +0300

    refactoring. draw_graphs

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ from constants import TIME_DELTA
 from monitor import Monitor
 
 
-def db_queries(monitor, start_time=None, db_name='ru_backend'):
-    """ Получает временной отрзок и делает запросы к DB """
+def monitoring(monitor, db_name='ru_backend'):
     config = ConfigParser()
     config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
     config.read(config_file)
@@ -23,9 +22,9 @@ def db_queries(monitor, start_time=None, db_name='ru_backend'):
                                  charset='utf8', cursorclass=DictCursor)) as connection:
         start_time = monitor.start_time.strftime('%Y-%m-%d %H:%M:%S')
         last_time = monitor.last_time.strftime('%Y-%m-%d %H:%M:%S')
-        logging.INFO('Выполняем запросы в DB')
+        logging.info('Выполняем запросы в DB')
         with connection.cursor() as cursor:
-            query = f"SELECT id, status, amount, returnsumm, create_ts FROM credit " \
+            query = f"SELECT id, status, create_ts FROM credit " \
                     f"where create_ts > '{start_time}' and create_ts < '{last_time}'"
             cursor.execute(query)
             credits = []
@@ -46,16 +45,15 @@ def db_queries(monitor, start_time=None, db_name='ru_backend'):
             for row in cursor:
                 statuses.append(row)
     # find metrics
-    logging.INFO('Рассчет метрик')
+    logging.info('Рассчет метрик')
     monitor.find_metrics(persons, statuses)
     # check and save credit and person stacks
     monitor.check_person_stacks(persons)
     monitor.check_credits_stack(credits)
-
-    #todo draw graphs
-    logging.INFO('Рисуем графики')
-
-    #todo update time
+    #draw graphs
+    logging.info('Рисуем графики')
+    monitor.draw_graphs()
+    monitor.update_time()
 
 
 
@@ -66,60 +64,30 @@ def main():
     format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s'
     logging.basicConfig(level=level, format=format, handlers=handlers)
     config = ConfigParser()
-    config.read('config.ini')
-    start_time = None
+    config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
+    config.read(config_file)
+    time_shift = None
     try:
         COMMAND_LINE_INPUT = eval(config['options']['COMMAND_LINE_INPUT'])
         if COMMAND_LINE_INPUT:
             if len(argv) > 1:
-                start_time = argv[1]
+                time_shift = argv[1]
+        else:
+            time_shift = 1
     except IndexError:
-        logging.exception('Введите правильное время!')
-        raise Exception('Введите правильное время!')
+        logging.exception('Введите количество часов для построения графика!')
+        raise Exception('Введите количество часов для построения графика!')
 
-    monitor = Monitor(start_time)
-    #todo change time interval
-    schedule.every(TIME_DELTA).seconds.do(db_queries, monitor=monitor, start_time=start_time)
-    #schedule.every(TIME_DELTA).seconds.do(db_queries, monitor=monitor, start_time=start_time, db_name='kz_backend')
+    monitor = Monitor(time_shift)
+    monitoring(monitor)
+
+    schedule.every(TIME_DELTA).minutes.do(monitoring, monitor=monitor)
+    #schedule.every(TIME_DELTA).minutes.do(monitoring, monitor=monitor, db_name='kz_backend')
 
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 if __name__ == '__main__':
-    #main()
-    config = ConfigParser()
-    config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
-    config.read(config_file)
-    monitor = Monitor()
-    with closing(pymysql.connect(host=config['db']['host'], port=int(config['db']['port']), user=config['db']['user'],
-                                 password=config['db']['password'], db='ru_backend',
-                                 charset='utf8', cursorclass=DictCursor)) as connection:
-        start_time = monitor.start_time.strftime('%Y-%m-%d %H:%M:%S')
-        last_time = monitor.last_time.strftime('%Y-%m-%d %H:%M:%S')
-        with connection.cursor() as cursor:
-            query = f"SELECT id, status, amount, returnsumm, create_ts, approved_ts FROM credit " \
-                    f"where create_ts > '{start_time}' and create_ts < '{last_time}'"
-            cursor.execute(query)
-            credits = []
-            for row in cursor:
-                credits.append(row)
-        with connection.cursor() as cursor:
-            query = f"SELECT id, stage, create_ts FROM person " \
-                    f"where create_ts > '{start_time}' and create_ts < '{last_time}'"
-            cursor.execute(query)
-            persons = []
-            for row in cursor:
-                persons.append(row)
-        with connection.cursor() as cursor:
-            query = f"SELECT credit_id, `from`, `to`, timestamp FROM h_credit_status " \
-                    f"where timestamp > '{start_time}' and timestamp < '{last_time}'"
-            cursor.execute(query)
-            statuses = []
-            for row in cursor:
-                statuses.append(row)
-        print('credit', credits)
-        print('person', persons)
-        print('hcs', statuses)
-        pass
+    main()
 
--- monitor.py
+++ monitor.py
@@ -1,50 +1,45 @@
 import datetime
 from decimal import Decimal
 from collections import deque
+
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 
 from constants import TIME_DELTA, STACK_DURATION
 
 
 class Monitor():
-    def __init__(self, start_time=None):
+    def __init__(self, time_shift=None):
         self.complete_bids_day = []         # Текущее количество заявок в стадии 6 за сутки (STACK_DURATION)
         self.incomplete_bids_day = []       # Текущее количество заявок в стадии < 6 за сутки (STACK_DURATION)
         self.inapproved_credits_day = []    # Текущее количество кредитов в статусах -1, 0 за сутки (STACK_DURATION)
-        self.complete_registration_day = [] # Текущий % прохождения цепочки за сутки (STACK_DURATION)
-        self.scoring_stuck_day = []         # Текущее количество кредитов зависших на скоринге за сутки (STACK_DURATION)
         ############## stacks
         self.stage_6_stack = deque()        # persons on stage 6 за сутки (STACK_DURATION)
         self.except_6_stack = deque()       # persons on stage < 6 за сутки (STACK_DURATION)
         self.scoring_stuck_stack = deque()  # credits with statuses 0 за сутки (STACK_DURATION)
         self.ids_stack = set()              # set айдишников из scoring_stuck_stack
         ############## metrics
+        self.complete_registration_day = [] # Текущий % прохождения цепочки за сутки (STACK_DURATION)
+        self.scoring_stuck_day = []         # Текущее количество кредитов зависших на скоринге за сутки (STACK_DURATION)
         self.new_bids = []                  # Количество новых заявок за TIME_DELTA
         self.approves = []                  # Количество одобрений за TIME_DELTA
-        self.amount = []                    # сумма выданных кредитов за TIME_DELTA
-        self.pastdue = []                   # количство уходов в просрочку за TIME_DELTA
-        self.pastdue_repayment = []         # количество выхода из просрочки за TIME_DELTA
-        self.scoring_time = []              # среднее время скоринга за TIME_DELTA
+        self.pastdue = []                   # Количество уходов в просрочку за TIME_DELTA
+        self.pastdue_repayment = []         # Количество выхода из просрочки за TIME_DELTA
+        self.scoring_time = []              # среднее время скоринга за TIME_DELTA - в минутах
         ############## под вопросом
         self.repeate_bids = []              # Количество повторных заявок за TIME_DELTA
         self.partner_bids = []              # количество заявок через партнеров за TIME_DELTA
 
         NOW = datetime.datetime.now()
-        if not start_time:
+        self.last_time = NOW
+        if not time_shift:
             self.start_time = NOW - datetime.timedelta(minutes=TIME_DELTA)
-            self.last_time = NOW
         else:
-            self.start_time = start_time
-            if not self.last_time:
-                self.last_time = NOW
-            else:
-                self.start_time = self.last_time
-                self.last_time = self.last_time + datetime.timedelta(minutes=TIME_DELTA)
+            self.start_time = NOW - datetime.timedelta(hours=int(time_shift))
 
     def find_metrics(self, persons, statuses):
         self.new_bids.append((self.last_time, len(persons)))
-
-        amount, pastdue, pastdue_repayment, approves, scoring_time = 0, 0, 0, 0, []
+        pastdue, pastdue_repayment, approves, scoring_time = 0, 0, 0, []
         for status in statuses:
             if status['to'] == 4:
                 pastdue += 1
@@ -57,15 +52,15 @@ class Monitor():
                     self.ids_stack.remove(status['credit_id'])
                     for credit in self.scoring_stuck_stack:
                         if credit['id'] == status['credit_id']:
-                            scoring_time.append(status['timestamp'] - credit['create_ts'])
-                            amount += credit['amount']
+                            # превращаем timedelta в количество минут
+                            delta = str(status['timestamp'] - credit['create_ts']).split(':')
+                            scoring_time.append(round(int(delta[0])*60 + int(delta[1]) + int(delta[2]) / 60, 1))
                             break
-        # todo convert to seconds?
-        self.scoring_time.append((self.last_time, sum(scoring_time) / len(scoring_time)))
+        if scoring_time:
+            self.scoring_time.append((self.last_time, round(sum(scoring_time) / len(scoring_time), 1)))
         self.approves.append((self.last_time, approves))
         self.pastdue.append((self.last_time, pastdue))
         self.pastdue_repayment.append((self.last_time, pastdue_repayment))
-        self.amount.append((self.last_time, amount))
 
     def check_person_stacks(self, persons):
         # добавляем новые заявки
@@ -77,10 +72,12 @@ class Monitor():
             else:
                 self.except_6_stack.append(person)
         # убираем просроченные заявки
-        while self.last_time - self.stage_6_stack[0]['create_ts'] > datetime.timedelta(hours=STACK_DURATION):
-            self.stage_6_stack.popleft()
-        while self.last_time - self.except_6_stack[0]['create_ts'] > datetime.timedelta(hours=STACK_DURATION):
-            self.except_6_stack.popleft()
+        if self.stage_6_stack:
+            while self.last_time - self.stage_6_stack[0]['create_ts'] > datetime.timedelta(hours=STACK_DURATION):
+                self.stage_6_stack.popleft()
+        if self.except_6_stack:
+            while self.last_time - self.except_6_stack[0]['create_ts'] > datetime.timedelta(hours=STACK_DURATION):
+                self.except_6_stack.popleft()
         # апдейтим количества заявок
         self.complete_bids_day.append((self.last_time, len(self.stage_6_stack)))
         self.incomplete_bids_day.append((self.last_time, len(self.except_6_stack)))
@@ -89,7 +86,6 @@ class Monitor():
         )
 
     def check_credits_stack(self, credits):
-
         # добавляем новые кредиты
         for credit in credits:
             if credit['status'] == 2 and credit in self.scoring_stuck_stack:
@@ -106,4 +102,56 @@ class Monitor():
         self.scoring_stuck_day.append((self.last_time, len(self.scoring_stuck_stack)))
 
     def draw_graphs(self):
-        pass
+        fig, ax = plt.subplots(figsize=(8, 6))
+        ax.set_title("Россия", fontsize=16)
+        ax.set_xlabel("Время", fontsize=14)
+        ax.grid(which="major", linewidth=1.2)
+        ax.grid(which="minor", linestyle="--", color="gray", linewidth=0.5)
+
+        plt.plot([i[0] for i in self.complete_registration_day],
+                 [i[1] for i in self.complete_registration_day], 'o-', label="Текущий % прохождения цепочки")
+        plt.plot([i[0] for i in self.scoring_stuck_day],
+                 [i[1] for i in self.scoring_stuck_day], 'o-', label="Текущее количество кредитов зависших на скоринге")
+        plt.plot([i[0] for i in self.new_bids],
+                 [i[1] for i in self.new_bids], 'o-', label="Количество новых заявок")
+        plt.plot([i[0] for i in self.approves],
+                 [i[1] for i in self.approves], 'o-', label="Количество одобрений")
+        plt.plot([i[0] for i in self.pastdue],
+                 [i[1] for i in self.pastdue], 'o-', label="Количество уходов в просрочку")
+        plt.plot([i[0] for i in self.pastdue_repayment],
+                 [i[1] for i in self.pastdue_repayment], 'o-', label="Количество выходов из просрочки")
+        plt.plot([i[0] for i in self.scoring_time],
+                 [i[1] for i in self.scoring_time], 'o-', label="Cреднее время скоринга в минутах")
+
+        ax.legend()
+        ax.xaxis.set_minor_locator(AutoMinorLocator())
+        ax.yaxis.set_minor_locator(AutoMinorLocator())
+        ax.tick_params(which='major', length=10, width=2)
+        ax.tick_params(which='minor', length=5, width=1)
+        plt.show()
+
+    def update_time(self):
+        self.start_time = self.last_time
+        self.last_time = self.last_time + datetime.timedelta(minutes=TIME_DELTA)
+
+if __name__ == '__main__':
+    bids = [(datetime.datetime(2020, 5, 3, 17, 58, 16), 3), (datetime.datetime(2020, 5, 3, 18, 3, 16), 4),
+            (datetime.datetime(2020, 5, 3, 18, 8, 16), 6), (datetime.datetime(2020, 5, 3, 18, 13, 16), 3),
+            (datetime.datetime(2020, 5, 3, 18, 18, 16), 2), (datetime.datetime(2020, 5, 3, 18, 23, 16), 5)]
+    credits = [(datetime.datetime(2020, 5, 3, 17, 58, 16), 6), (datetime.datetime(2020, 5, 3, 18, 3, 16), 17),
+            (datetime.datetime(2020, 5, 3, 18, 8, 16), 26), (datetime.datetime(2020, 5, 3, 18, 13, 16), 4),
+            (datetime.datetime(2020, 5, 3, 18, 18, 16), 7), (datetime.datetime(2020, 5, 3, 18, 23, 16), 8)]
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.set_title("Россия", fontsize=16)
+    ax.set_xlabel("Время", fontsize=14)
+    ax.grid(which="major", linewidth=1.2)
+    ax.grid(which="minor", linestyle="--", color="gray", linewidth=0.5)
+    plt.plot([i[0] for i in bids], [i[1] for i in bids], 'o-', label="bids")
+    plt.plot([i[0] for i in credits], [i[1] for i in credits],'o-', label="credits")
+    ax.legend()
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
+    ax.tick_params(which='major', length=10, width=2)
+    ax.tick_params(which='minor', length=5, width=1)
+
+    plt.show()
